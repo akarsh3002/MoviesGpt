2dd770c5e8ac99f72b0829e5177b9fdf7b44e1f0
akarsh3002 <akarshsrivastava3@gmail.com>
2026-09-05 16:41:01 +0530

    added react player

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,9 +1,9 @@
 const VideoTitle = ({ title, description }) => {
   return (
     <div className="absolute inset-0 px-12 pt-[20%] text-white bg-linear-to-r from-black/90 via-black/40 to-transparent">
-      <h1 className="text-2xl font-bold">{title}</h1>
+      <h1 className="text-xl font-bold">{title}</h1>
 
-      <p className="py-6 text-sm w-1/4">{description}</p>
+      <p className="py-4 text-sm font-light w-1/4">{description}</p>
 
       <div className="flex gap-4">
         <button className="bg-white hover:bg-gray-200 text-black font-bold py-2 px-6 rounded">
